861d935582542e90c81f46f0953f49e43f4b8620
Auriane Martel <auriane.martel@gmail.com>
2025-01-15 16:28:50 +0100

    Modif visu.py

--- Tests_visu_gcode/visu.py
+++ Tests_visu_gcode/visu.py
@@ -41,6 +41,14 @@ def parse_gcode(gcode):
                 x, y = new_x, new_y
             else:
                 print("Ne matche pas >> G2" + ligne)
+        elif ligne.startswith('G3'):
+            match = re.match(r'G3\s+X([\d.-]+)\s+Y([\d.-]+)\s+I([\d.-]+)\s+J([\d.-]+)', ligne)
+            if match:
+                new_x, new_y, i, j = map(float, match.groups())
+                mouvements.append({'type': 'G3', 'x1': x, 'y1': y, 'x2': new_x, 'y2': new_y, 'i': i, 'j': j})
+                x, y = new_x, new_y
+            else:
+                print("Ne matche pas >> G2" + ligne)
     print(json.dumps(mouvements, indent=4, sort_keys=True))
     return mouvements
 
@@ -50,7 +58,17 @@ def plot_gcode(mouvements):
     for mouvement in mouvements:
         if mouvement['type'] == 'G0':
             plt.plot([mouvement['x1'], mouvement['x2']], [mouvement['y1'], mouvement['y2']], 'b--', label='G0' if 'G0' not in plt.gca().get_legend_handles_labels()[1] else "") # bleu pointillé
-        elif mouvement['type'] == 'G2':
+        elif mouvement['type'] == 'G2': #Modifier sens de rotation pour G2
+            centre_x = mouvement['x1'] + mouvement['i']
+            centre_y = mouvement['y1'] + mouvement['j']
+            rayon = ((mouvement['i'])**2 + (mouvement['j'])**2)**0.5
+            start_angle = np.arctan2(mouvement['y2']-centre_y, mouvement['x2']-centre_x) * 180 / np.pi
+            end_angle = np.arctan2(mouvement['y1']-centre_y, mouvement['x1']-centre_x) * 180 / np.pi
+            if mouvement['x1'] == mouvement['x2'] and mouvement['y1'] == mouvement['y2']:
+              plt.gca().add_patch(patches.Circle((centre_x, centre_y), rayon, fill=False, color='r', label='G2 Cercle' if 'G2 Cercle' not in plt.gca().get_legend_handles_labels()[1] else ""))
+            else:
+              plt.gca().add_patch(patches.Arc((centre_x, centre_y), 2*rayon, 2*rayon, theta1=start_angle, theta2=end_angle, color='r', label='G2 Arc' if 'G2 Arc' not in plt.gca().get_legend_handles_labels()[1] else "")) # rouge
+        elif mouvement['type'] == 'G3':
             centre_x = mouvement['x1'] + mouvement['i']
             centre_y = mouvement['y1'] + mouvement['j']
             rayon = ((mouvement['i'])**2 + (mouvement['j'])**2)**0.5
@@ -60,6 +78,7 @@ def plot_gcode(mouvements):
               plt.gca().add_patch(patches.Circle((centre_x, centre_y), rayon, fill=False, color='r', label='G2 Cercle' if 'G2 Cercle' not in plt.gca().get_legend_handles_labels()[1] else ""))
             else:
               plt.gca().add_patch(patches.Arc((centre_x, centre_y), 2*rayon, 2*rayon, theta1=start_angle, theta2=end_angle, color='r', label='G2 Arc' if 'G2 Arc' not in plt.gca().get_legend_handles_labels()[1] else "")) # rouge
+    
     plt.xlabel('X')
     plt.ylabel('Y')
     plt.title('Visualisation du G-code')
