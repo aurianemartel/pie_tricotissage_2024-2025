76884bf8efe595faa984240ba46b17656b9c9784
Alexis DE BRITO <debritoalexi@gmail.com>
2025-05-03 16:51:29 +0200

    Suppression de commentaires superflus

--- detpts/main_det_pts.py
+++ detpts/main_det_pts.py
@@ -4,4 +4,3 @@ if __name__ == '__main__':
     chemin = "4gpes.png"
     lx, ly, longueurs, pmpg = det_trace.detection_trace(chemin, epsilon=0.01, afficher_im_init=False, afficher_squelette=False)
     _, longueur_min = generer_pos_aig.generer_pos_aiguilles(pmpg, 1, 0, 0, [10 for i in range(len(pmpg))],"4gpes.png", afficher_points_pre_scale=False)
-    #print(longueur_min)
